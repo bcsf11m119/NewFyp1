f57868a64dffe3ff8352b35c5a6d8cc10813a1d5
bcsf11m119 <bcsf11m119@users.noreply.github.com>
2015-05-27 11:39:33 +0500

    Update LocationController.cs

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -45,9 +45,62 @@ namespace WebApplicationFyp.Controllers
             return c;
         }
         // POST api/<controller>
-        public void Post([FromBody]string value)
+        public string PostMessage(Location m)
         {
+
+            //var c = db.Children.Find(m.Cid);
+            //if (!c.Equals(null))
+            //{
+            //    //c.CallLogs.Add(m);
+            //    db.Locations.Add(m);
+            //    db.SaveChanges();
+            //    return "location record successfully added";
+            //}
+
+            var c = from x in db.Children
+                    //where x.Number.Equals(id.ToString())
+                    where x.Cid == m.Cid
+                    select x;
+            
+            try
+            {
+                // Your code...
+                // Could also be before try if you know the exception occurs in SaveChanges
+
+                if (c != null)
+                {
+                    Location l = new Location();
+                    l.Cid = m.Cid;
+                    l.Date = m.Date;
+                    l.Duration = m.Duration;
+                    l.Time = m.Time;
+                    l.Location1 = m.Location1;
+                    db.Locations.Add(l);
+                    db.SaveChanges();
+                }
+
+                
+                return "location record successfully added";
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw e;
+            }
+                
+            
+            //return null;
         }
+
 
         // PUT api/<controller>/5
         public void Put(int id, [FromBody]string value)
